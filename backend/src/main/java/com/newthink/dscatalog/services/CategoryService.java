3bd739c238509a669cdc4e8a143f1f8faa4a2631
Elias Mendes <elias.mendes.2009@gmail.com>
2021-11-10 22:32:48 -0300

    H2 Database

---
+++ backend/src/main/java/com/newthink/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.newthink.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.newthink.dscatalog.entities.Category;
+import com.newthink.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
