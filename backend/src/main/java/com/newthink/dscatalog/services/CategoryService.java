1f6c6ad6598d9cee83ef4b08dd12fd1ced65142c
Elias Mendes <elias.mendes.2009@gmail.com>
2021-11-11 21:09:31 -0300

    DTO

--- backend/src/main/java/com/newthink/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/newthink/dscatalog/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.newthink.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.newthink.dscatalog.entities.Category;
+import com.newthink.dscatalog.dto.CategoryDTO;
 import com.newthink.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -14,8 +15,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		return repository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList());
 	}
 
 }
