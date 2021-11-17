db444ed3c841441992f22da8077b92ff03d78840
Elias Mendes <elias.mendes.2009@gmail.com>
2021-11-16 21:42:24 -0300

    Auditing data

--- backend/src/main/java/com/newthink/dscatalog/entities/Category.java
+++ backend/src/main/java/com/newthink/dscatalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.newthink.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +23,22 @@ public class Category implements Serializable{
 	private Long id;
 	
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();	
+	}
 	
 	public Category() {
 	}
@@ -42,6 +62,14 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updateAt;
 	}
 
 	@Override
